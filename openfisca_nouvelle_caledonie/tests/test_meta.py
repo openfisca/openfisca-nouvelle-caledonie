8e4c2d7d8614973fc436d28eb571539bdfd9ce6d
Thomas Guillet <thomas@palya.fr>
2025-11-02 20:41:20 +0100

    Extrait les variables de primes du public dans un fichier dédié

---
+++ openfisca_nouvelle_caledonie/tests/test_meta.py
@@ -0,0 +1,7 @@
+from .. import CountryTaxBenefitSystem
+
+def test_metadata():
+    tbs = CountryTaxBenefitSystem()
+    metadata = tbs.get_package_metadata()
+    assert metadata['name'] == 'openfisca-nouvelle-caledonie'
+    assert metadata['repository_url'] == 'https://github.com/openfisca/openfisca-nouvelle-caledonie'
